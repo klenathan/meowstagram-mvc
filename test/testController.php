88ffb90c0c79e1c5dc76da0b2ab8f1620c1b1424
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2022-06-24 21:38:49 +0700

    Update testController.php

--- test/testController.php
+++ test/testController.php
@@ -1,6 +1,7 @@
 <?php
 class testController {
     function __construct () {
+        // test controller
         include_once("testView.php");
         if($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "Post";
